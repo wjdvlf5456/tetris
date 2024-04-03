3936cf192f10ad3fe3115fd2aa57ce4cf3f293ba
Lee Sangyeop <cstria0106@gmail.com>
2024-04-03 13:10:00 +0900

    add name to tetromino

--- tetromino.h
+++ tetromino.h
@@ -9,6 +9,9 @@
 class Tetromino {
   static const int MAX_SIZE = 4;
 
+  // 디버그용 이름
+  std::string name_;
+
   // 테트로미노의 모양을 저장하는 배열
   // shape[x][y]가 true인 경우 x, y 위치에 블록이 있는 것이고 false의 경우 없는
   // 것이다.
@@ -31,7 +34,7 @@ public:
   // OOO
   // XXX
   //
-  // Tetromino T(3, "XOXOOOXXX")
+  // Tetromino T("T", 3, "XOXOOOXXX")
 
   // I 미노의 경우 다음과 같다.
   // XXXX
@@ -39,8 +42,11 @@ public:
   // XXXX
   // XXXX
   //
-  // Tetromino I(4, "XXXXOOOOXXXXXXXX")
-  Tetromino(int size, std::string shape);
+  // Tetromino I("I", 4, "XXXXOOOOXXXXXXXX")
+  Tetromino(std::string name, int size, std::string shape);
+
+  // 이름을 반환한다.
+  std::string name() { return name_; }
 
   // 테트로미노의 사이즈를 반환한다.
   int size() { return size_; }
