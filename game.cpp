8d2ca25fec8b5c9cb83f1643319b41ddfab12c27
최정필 <wjdvlf1407@gmail.com>
2024-04-05 21:19:39 +0900

    xx

--- game.cpp
+++ game.cpp
@@ -1,6 +1,249 @@
 #include "game.h"
 
+Game::Game() {
+    // 게임 초기화
+    currentTetromino_ = Tetromino::randomTetromino();
+    nextTetromino_ = Tetromino::randomTetromino();
+    holdTetromino_ = Tetromino();
+    shadowTetromino_ = currentTetromino_;
+    dropDelayCounter_ = 0;
+    linesCleared_ = 0;
+    gameOver_ = false;
+    hardDrop_ = false;
+}
 
+void Game::update() {
+    // 게임 업데이트
+    handleInput();
+    if (!gameOver_) {
+        if (dropDelayCounter_ >= DROP_DELAY || hardDrop_) {
+            if (!moveTetrominoDown()) {
+                // 테트로미노가 더이상 움직일 수 없음
+                lockTetromino();
+                clearLines();
+                spawnNewTetromino();
+                if (!isValidPosition(currentTetromino_, currentX_, currentY_)) {
+                    // 새로운 테트로미노가 생성되지 않을 경우, 게임 종료
+                    gameOver_ = true;
+                }
+            }
+            dropDelayCounter_ = 0;
+            hardDrop_ = false;
+        } else {
+            ++dropDelayCounter_;
+        }
+    }
+}
 
+void Game::draw() {
+    // 게임 화면 그리기
+    console::clear();
+    drawBoard();
+    drawTetromino(currentTetromino_, currentX_, currentY_);
+    drawNextTetromino();
+    drawHoldTetromino();
+    drawShadowTetromino();
+    drawStats();
+}
 
+void Game::handleInput() {
+    // 입력 처리
+    if (console::key(console::K_LEFT)) {
+        moveTetrominoLeft();
+    }
+    if (console::key(console::K_RIGHT)) {
+        moveTetrominoRight();
+    }
+    if (console::key(console::K_DOWN)) {
+        softDropTetromino();
+    }
+    if (console::key(console::K_UP)) {
+        rotateTetrominoCW();
+    }
+    if (console::key(console::K_Z)) {
+        rotateTetrominoCCW();
+    }
+    if (console::key(console::K_SPACE)) {
+        hardDrop_ = true;
+    }
+    if (console::key(console::K_ESC)) {
+        gameOver_ = true;
+    }
+    if (console::key(console::K_X)) {
+        holdTetromino();
+    }
+}
 
+void Game::drawBoard() {
+    // 게임 보드 그리기
+    for (int y = 0; y < BOARD_HEIGHT; ++y) {
+        for (int x = 0; x < BOARD_WIDTH; ++x) {
+            if (board_[x][y]) {
+                console::draw(x, y, BLOCK_STRING);
+            }
+        }
+    }
+}
+
+void Game::drawTetromino(const Tetromino& tetromino, int x, int y) {
+    // 테트로미노 그리기
+    tetromino.drawAt(SNAKE_STRING, x, y);
+}
+
+void Game::drawNextTetromino() {
+    // 넥스트 테트로미노 그리기
+    console::draw(12, 1, "NEXT:");
+    nextTetromino_.drawAt(SNAKE_STRING, 12, 3);
+}
+
+void Game::drawHoldTetromino() {
+    // 홀드 테트로미노 그리기
+    console::draw(12, 8, "HOLD:");
+    holdTetromino_.drawAt(SNAKE_STRING, 12, 10);
+}
+
+void Game::drawShadowTetromino() {
+    // 그림자 테트로미노 그리기
+    int shadowY = currentY_;
+    while (isValidPosition(currentTetromino_, currentX_, shadowY + 1)) {
+        ++shadowY;
+    }
+    shadowTetromino_.drawAt(SHADOW_STRING, currentX_, shadowY);
+}
+
+void Game::drawStats() {
+    // 게임 상태 표시
+    console::draw(12, 16, "LINES:");
+    console::draw(12, 18, std::to_string(linesCleared_));
+    console::draw(12, 20, "TIME:");
+    console::draw(12, 22, std::to_string(gameTime_));
+}
+
+bool Game::isValidPosition(const Tetromino& tetromino, int x, int y) {
+    // 위치가 유효한지 확인
+    for (int j = 0; j < tetromino.size(); ++j) {
+        for (int i = 0; i < tetromino.size(); ++i) {
+            if (tetromino.check(i, j)) {
+                int boardX = x + i;
+                int boardY = y + j;
+                if (boardX < 0 || boardX >= BOARD_WIDTH || boardY < 0 || boardY >= BOARD_HEIGHT || board_[boardX][boardY]) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+bool Game::moveTetrominoLeft() {
+    // 테트로미노를 왼쪽으로 이동
+    if (isValidPosition(currentTetromino_, currentX_ - 1, currentY_)) {
+        --currentX_;
+        return true;
+    }
+    return false;
+}
+
+bool Game::moveTetrominoRight() {
+    // 테트로미노를 오른쪽으로 이동
+    if (isValidPosition(currentTetromino_, currentX_ + 1, currentY_)) {
+        ++currentX_;
+        return true;
+    }
+    return false;
+}
+
+bool Game::moveTetrominoDown() {
+    // 테트로미노를 아래로 이동
+    if (isValidPosition(currentTetromino_, currentX_, currentY_ + 1)) {
+        ++currentY_;
+        return true;
+    }
+    return false;
+}
+
+void Game::softDropTetromino() {
+    // 소프트 드롭
+    while (moveTetrominoDown());
+}
+
+void Game::rotateTetrominoCW() {
+    // 시계 방향 회전
+    Tetromino rotated = currentTetromino_.rotatedCW();
+    if (isValidPosition(rotated, currentX_, currentY_)) {
+        currentTetromino_ = rotated;
+    }
+}
+
+void Game::rotateTetrominoCCW() {
+    // 반시계 방향 회전
+    Tetromino rotated = currentTetromino_.rotatedCCW();
+    if (isValidPosition(rotated, currentX_, currentY_)) {
+        currentTetromino_ = rotated;
+    }
+}
+
+void Game::lockTetromino() {
+    // 테트로미노 고정
+    for (int j = 0; j < currentTetromino_.size(); ++j) {
+        for (int i = 0; i < currentTetromino_.size(); ++i) {
+            if (currentTetromino_.check(i, j)) {
+                int boardX = currentX_ + i;
+                int boardY = currentY_ + j;
+                if (boardX >= 0 && boardX < BOARD_WIDTH && boardY >= 0 && boardY < BOARD_HEIGHT) {
+                    board_[boardX][boardY] = true;
+                }
+            }
+        }
+    }
+}
+
+void Game::clearLines() {
+    // 줄 삭제
+    int linesClearedThisRound = 0;
+    for (int y = BOARD_HEIGHT - 1; y >= 0; --y) {
+        bool lineFull = true;
+        for (int x = 0; x < BOARD_WIDTH; ++x) {
+            if (!board_[x][y]) {
+                lineFull = false;
+                break;
+            }
+        }
+        if (lineFull) {
+            ++linesClearedThisRound;
+            for (int j = y; j > 0; --j) {
+                for (int i = 0; i < BOARD_WIDTH; ++i) {
+                    board_[i][j] = board_[i][j - 1];
+                }
+            }
+            for (int i = 0; i < BOARD_WIDTH; ++i) {
+                board_[i][0] = false;
+            }
+            ++y; // 다음 줄도 확인해야 함
+        }
+    }
+    linesCleared_ += linesClearedThisRound;
+}
+
+void Game::spawnNewTetromino() {
+    // 새로운 테트로미노 생성
+    currentTetromino_ = nextTetromino_;
+    nextTetromino_ = Tetromino::randomTetromino();
+    currentX_ = BOARD_WIDTH / 2 - currentTetromino_.size() / 2;
+    currentY_ = 0;
+    shadowTetromino_ = currentTetromino_;
+}
+
+void Game::holdTetromino() {
+    // 테트로미노 홀드
+    if (!holdUsed_) {
+        Tetromino temp = currentTetromino_;
+        if (holdTetromino_.name() != "") {
+            currentTetromino_ = holdTetromino_;
+        } else {
+            spawnNewTetromino();
+        }
+        holdTetromino_ = temp;
+        holdUsed_ = true;
+    }
+}
--- tetromino.cpp
+++ tetromino.cpp
@@ -1,1 +1,60 @@
 #include "tetromino.h"
+
+Tetromino Tetromino::I("I", 4, "XXXX");
+Tetromino Tetromino::O("O", 2, "XX"
+                                  "XX");
+Tetromino Tetromino::T("T", 3, " X "
+                                  "XXX");
+Tetromino Tetromino::S("S", 3, " XX"
+                                  "XX ");
+Tetromino Tetromino::Z("Z", 3, "XX "
+                                  " XX");
+Tetromino Tetromino::J("J", 3, "X  "
+                                  "XXX");
+Tetromino Tetromino::L("L", 3, "  X"
+                                  "XXX");
+
+Tetromino::Tetromino(std::string name, int size, std::string shape) : name_(name), size_(size) {
+    // 초기화
+    int index = 0;
+    for (int y = 0; y < size_; ++y) {
+        for (int x = 0; x < size_; ++x) {
+            shape_[x][y] = (shape[index] == 'X');
+            ++index;
+        }
+    }
+    original_ = this;
+}
+
+Tetromino Tetromino::rotatedCW() {
+    // 시계 방향 회전
+    Tetromino rotated(name_, size_, "");
+    for (int y = 0; y < size_; ++y) {
+        for (int x = 0; x < size_; ++x) {
+            rotated.shape_[y][size_ - 1 - x] = shape_[x][y];
+        }
+    }
+    return rotated;
+}
+
+Tetromino Tetromino::rotatedCCW() {
+    // 반시계 방향 회전
+    Tetromino rotated(name_, size_, "");
+    for (int y = 0; y < size_; ++y) {
+        for (int x = 0; x < size_; ++x) {
+            rotated.shape_[size_ - 1 - y][x] = shape_[x][y];
+        }
+    }
+    return rotated;
+}
+
+void Tetromino::drawAt(std::string s, int x, int y) {
+    // 테트로미노를 그린다.
+    for (int j = 0; j < size_; ++j) {
+        for (int i = 0; i < size_; ++i) {
+            if (check(i, j)) {
+                console::draw(x + i, y + j, s);
+            }
+        }
+    }
+}
